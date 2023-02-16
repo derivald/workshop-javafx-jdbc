eb83570fd606d43c1c8c2e2a00d1a1556ef3001d
Derivaldo Brito <derivaldo.brito@yahoo.com.br>
2023-02-16 17:56:11 -0300

    DepartmentList controller

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -61,7 +61,7 @@ public class MainViewController implements Initializable{
 		mainVBox.getChildren().addAll(newVBox.getChildren());
 		
 		} catch (IOException e) {
-			Alerts.showAlert("IO Exception", "Error loadin view",  e.getMessage(),AlertType.ERROR);
+			Alerts.showAlert("IO Exception", "Error loading view",  e.getMessage(),AlertType.ERROR);
 		}
 	}
 
